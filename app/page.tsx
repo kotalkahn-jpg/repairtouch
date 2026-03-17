7edf4efb3bede754607acc5792551fda7765c817
Rowland Banda <rowlandbanda2003@gmail.com>
2026-03-17 15:11:47 +0200

    removed loader code in frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react"
-
-import Loader from "@/components/Loader"
-
 import HeroCarousel from "@/components/sections/HeroCarousel"
 import ServicesSection from "@/components/sections/ServicesSection"
 import BusinessSolutions from "@/components/sections/BusinessSolutions"
@@ -13,44 +9,12 @@ import CTASection from "@/components/sections/CTASection"
 
 export default function Home() {
 
-  const [assetsLoaded, setAssetsLoaded] = useState(false)
-  const [minTimePassed, setMinTimePassed] = useState(false)
-
-  useEffect(() => {
-
-    // MINIMUM LOADER TIME
-    const timer = setTimeout(() => {
-      setMinTimePassed(true)
-    }, 5000)
-
-    // PAGE ASSETS
-    const handleLoad = () => {
-      setAssetsLoaded(true)
-    }
-
-    if (document.readyState === "complete") {
-      handleLoad()
-    } else {
-      window.addEventListener("load", handleLoad)
-    }
-
-    return () => {
-      clearTimeout(timer)
-      window.removeEventListener("load", handleLoad)
-    }
-
-  }, [])
-
-  const loading = !(assetsLoaded && minTimePassed)
-
   return (
 
     <>
-      <Loader visible={loading} />
 
       <main
-        className={`transition-opacity duration-700
-        ${loading ? "opacity-0" : "opacity-100"}`}
+        className="transition-opacity duration-700 opacity-100"
       >
 
         <HeroCarousel />
